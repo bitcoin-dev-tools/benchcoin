e3401efbfd25fe0c4300d51a2a54c7bf7c43bf1d
Lőrinc <pap.lorinc@gmail.com>
2024-11-28 18:39:06 +0100

    Use fread_unlocked on Linux
    |               ns/op |                op/s |    err% |          ins/op |          cyc/op |    IPC |         bra/op |   miss% |     total | benchmark
    |--------------------:|--------------------:|--------:|----------------:|----------------:|-------:|---------------:|--------:|----------:|:----------
    |        6,643,974.15 |              150.51 |    0.0% |   69,866,666.72 |   22,903,202.14 |  3.051 |   4,778,910.55 |    0.4% |     10.60 | `ReadBlockFromDiskTest`

--- src/streams.cpp
+++ src/streams.cpp
@@ -23,7 +23,7 @@ AutoFile::AutoFile(std::FILE* file, std::vector<std::byte> data_xor)
 std::size_t AutoFile::detail_fread(Span<std::byte> dst)
 {
     if (!m_file) throw std::ios_base::failure("AutoFile::read: file handle is nullptr");
-    size_t ret = std::fread(dst.data(), 1, dst.size(), m_file);
+    size_t ret = FRead(dst.data(), dst.size(), m_file);
     if (!m_xor.empty()) {
         if (!m_position.has_value()) throw std::ios_base::failure("AutoFile::read: position unknown");
         util::Xor(dst.subspan(0, ret), m_xor, *m_position);

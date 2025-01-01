willcl-ark/bitcoin

cache CTransaction's serialized size (with/no witness)

src/consensus/tx_check.cpp
@@ -16,7 +16,7 @@ bool CheckTransaction(const CTransaction& tx, TxValidationState& state)
     if (tx.vout.empty())
         return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-vout-empty");
     // Size limits (this doesn't take the witness into account, as that hasn't been checked for malleability)
-    if (::GetSerializeSize(TX_NO_WITNESS(tx)) * WITNESS_SCALE_FACTOR > MAX_BLOCK_WEIGHT) {
+    if (tx.GetStrippedSize() * WITNESS_SCALE_FACTOR > MAX_BLOCK_WEIGHT) {
         return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-oversize");
     }
 

src/consensus/validation.h
@@ -132,7 +132,7 @@ class BlockValidationState : public ValidationState<BlockValidationResult> {};
 // weight = (stripped_size * 3) + total_size.
 static inline int32_t GetTransactionWeight(const CTransaction& tx)
 {
-    return ::GetSerializeSize(TX_NO_WITNESS(tx)) * (WITNESS_SCALE_FACTOR - 1) + ::GetSerializeSize(TX_WITH_WITNESS(tx));
+    return tx.GetStrippedSize() * (WITNESS_SCALE_FACTOR - 1) + tx.GetTotalSize();
 }
 static inline int64_t GetBlockWeight(const CBlock& block)
 {

src/test/miner_tests.cpp
@@ -148,7 +148,7 @@ void MinerTestingSetup::TestPackageSelection(const CScript& scriptPubKey, const 
     tx.vout[0].nValue = 5000000000LL - 1000 - 50000; // 0 fee
     Txid hashFreeTx = tx.GetHash();
     AddToMempool(tx_mempool, entry.Fee(0).FromTx(tx));
-    size_t freeTxSize = ::GetSerializeSize(TX_WITH_WITNESS(tx));
+    size_t freeTxSize = CTransaction{tx}.GetTotalSize();
 
     // Calculate a fee on child transaction that will put the package just
     // below the block min tx fee (assuming 1 child tx of the same size).

src/test/transaction_tests.cpp
@@ -389,7 +389,7 @@ BOOST_AUTO_TEST_CASE(tx_oversized)
         return CTransaction(tx);
     };
     const auto maxTransactionSize = MAX_BLOCK_WEIGHT / WITNESS_SCALE_FACTOR;
-    const auto oversizedTransactionBaseSize = ::GetSerializeSize(TX_NO_WITNESS(createTransaction(maxTransactionSize))) - maxTransactionSize;
+    const auto oversizedTransactionBaseSize = createTransaction(maxTransactionSize).GetStrippedSize() - maxTransactionSize;
 
     auto maxPayloadSize = maxTransactionSize - oversizedTransactionBaseSize;
     {

src/test/validation_tests.cpp
@@ -250,7 +250,7 @@ BOOST_AUTO_TEST_CASE(block_malleation)
             block.vtx.push_back(MakeTransactionRef(mtx));
             block.hashMerkleRoot = block.vtx.back()->GetHash();
             assert(block.vtx.back()->IsCoinBase());
-            assert(GetSerializeSize(TX_NO_WITNESS(block.vtx.back())) == 64);
+            assert(block.vtx.back()->GetStrippedSize() == 64);
         }
         BOOST_CHECK(is_not_mutated(block, /*check_witness_root=*/false));
     }
@@ -281,13 +281,12 @@ BOOST_AUTO_TEST_CASE(block_malleation)
         CMutableTransaction tx3;
         BOOST_CHECK(DecodeHexTx(tx3, "cdaf22d00002c6a7f848f8ae4d30054e61dcf3303d6fe01d282163341f06feecc10032b3160fcab87bdfe3ecfb769206ef2d991b92f8a268e423a6ef4d485f06", /*try_no_witness=*/true, /*try_witness=*/false));
         {
-            // Verify that double_sha256(txid1||txid2) == txid3
             HashWriter hasher;
             hasher.write(tx1.GetHash());
             hasher.write(tx2.GetHash());
             assert(hasher.GetHash() == tx3.GetHash());
-            // Verify that tx3 is 64 bytes in size (without witness).
-            assert(GetSerializeSize(TX_NO_WITNESS(tx3)) == 64);
+
+            assert(CTransaction{tx3}.GetStrippedSize() == 64);
         }
 
         CBlock block;

src/validation.cpp
@@ -793,7 +793,7 @@ bool MemPoolAccept::PreChecks(ATMPArgs& args, Workspace& ws)
     }
 
     // Transactions smaller than 65 non-witness bytes are not relayed to mitigate CVE-2017-12842.
-    if (::GetSerializeSize(TX_NO_WITNESS(tx)) < MIN_STANDARD_TX_NONWITNESS_SIZE)
+    if (tx.GetStrippedSize() < MIN_STANDARD_TX_NONWITNESS_SIZE)
         return state.Invalid(TxValidationResult::TX_NOT_STANDARD, "tx-size-small");
 
     // Only accept nLockTime-using transactions that can be mined in the next
@@ -4143,8 +4143,7 @@ bool IsBlockMutated(const CBlock& block, bool check_witness_root)
         //
         // Note: This is not a consensus change as this only applies to blocks that
         // don't have a coinbase transaction and would therefore already be invalid.
-        return std::any_of(block.vtx.begin(), block.vtx.end(),
-                           [](auto& tx) { return GetSerializeSize(TX_NO_WITNESS(tx)) == 64; });
+        return std::ranges::any_of(block.vtx, [](auto& tx) { return tx->GetStrippedSize() == 64; });
     } else {
         // Theoretically it is still possible for a block with a 64 byte
         // coinbase transaction to be mutated but we neglect that possibility

bitcoin-dev-tools/benchcoin

src/consensus/tx_check.cpp
@@ -16,7 +16,7 @@ bool CheckTransaction(const CTransaction& tx, TxValidationState& state)
     if (tx.vout.empty())
         return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-vout-empty");
     // Size limits (this doesn't take the witness into account, as that hasn't been checked for malleability)
-    if (::GetSerializeSize(TX_NO_WITNESS(tx)) * WITNESS_SCALE_FACTOR > MAX_BLOCK_WEIGHT) {
+    if (tx.SizeNoWitness() * WITNESS_SCALE_FACTOR > MAX_BLOCK_WEIGHT) {
         return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-oversize");
     }
 

src/consensus/validation.h
@@ -132,7 +132,7 @@ class BlockValidationState : public ValidationState<BlockValidationResult> {};
 // weight = (stripped_size * 3) + total_size.
 static inline int32_t GetTransactionWeight(const CTransaction& tx)
 {
-    return ::GetSerializeSize(TX_NO_WITNESS(tx)) * (WITNESS_SCALE_FACTOR - 1) + ::GetSerializeSize(TX_WITH_WITNESS(tx));
+    return tx.SizeNoWitness() * (WITNESS_SCALE_FACTOR - 1) + tx.SizeWithWitness();
 }
 static inline int64_t GetBlockWeight(const CBlock& block)
 {

src/test/miner_tests.cpp
@@ -148,7 +148,7 @@ void MinerTestingSetup::TestPackageSelection(const CScript& scriptPubKey, const 
     tx.vout[0].nValue = 5000000000LL - 1000 - 50000; // 0 fee
     Txid hashFreeTx = tx.GetHash();
     AddToMempool(tx_mempool, entry.Fee(0).FromTx(tx));
-    size_t freeTxSize = ::GetSerializeSize(TX_WITH_WITNESS(tx));
+    size_t freeTxSize = CTransaction{tx}.SizeWithWitness();
 
     // Calculate a fee on child transaction that will put the package just
     // below the block min tx fee (assuming 1 child tx of the same size).

src/test/transaction_tests.cpp
@@ -389,7 +389,7 @@ BOOST_AUTO_TEST_CASE(tx_oversized)
         return CTransaction(tx);
     };
     const auto maxTransactionSize = MAX_BLOCK_WEIGHT / WITNESS_SCALE_FACTOR;
-    const auto oversizedTransactionBaseSize = ::GetSerializeSize(TX_NO_WITNESS(createTransaction(maxTransactionSize))) - maxTransactionSize;
+    const auto oversizedTransactionBaseSize = createTransaction(maxTransactionSize).SizeNoWitness() - maxTransactionSize;
 
     auto maxPayloadSize = maxTransactionSize - oversizedTransactionBaseSize;
     {

src/test/validation_tests.cpp
@@ -250,7 +250,7 @@ BOOST_AUTO_TEST_CASE(block_malleation)
             block.vtx.push_back(MakeTransactionRef(mtx));
             block.hashMerkleRoot = block.vtx.back()->GetHash();
             assert(block.vtx.back()->IsCoinBase());
-            assert(GetSerializeSize(TX_NO_WITNESS(block.vtx.back())) == 64);
+            assert(block.vtx.back()->SizeNoWitness() == 64);
         }
         BOOST_CHECK(is_not_mutated(block, /*check_witness_root=*/false));
     }
@@ -281,13 +281,12 @@ BOOST_AUTO_TEST_CASE(block_malleation)
         CMutableTransaction tx3;
         BOOST_CHECK(DecodeHexTx(tx3, "cdaf22d00002c6a7f848f8ae4d30054e61dcf3303d6fe01d282163341f06feecc10032b3160fcab87bdfe3ecfb769206ef2d991b92f8a268e423a6ef4d485f06", /*try_no_witness=*/true, /*try_witness=*/false));
         {
-            // Verify that double_sha256(txid1||txid2) == txid3
             HashWriter hasher;
             hasher.write(tx1.GetHash());
             hasher.write(tx2.GetHash());
             assert(hasher.GetHash() == tx3.GetHash());
-            // Verify that tx3 is 64 bytes in size (without witness).
-            assert(GetSerializeSize(TX_NO_WITNESS(tx3)) == 64);
+
+            assert(CTransaction{tx3}.SizeNoWitness() == 64);
         }
 
         CBlock block;

src/validation.cpp
@@ -793,7 +793,7 @@ bool MemPoolAccept::PreChecks(ATMPArgs& args, Workspace& ws)
     }
 
     // Transactions smaller than 65 non-witness bytes are not relayed to mitigate CVE-2017-12842.
-    if (::GetSerializeSize(TX_NO_WITNESS(tx)) < MIN_STANDARD_TX_NONWITNESS_SIZE)
+    if (tx.SizeNoWitness() < MIN_STANDARD_TX_NONWITNESS_SIZE)
         return state.Invalid(TxValidationResult::TX_NOT_STANDARD, "tx-size-small");
 
     // Only accept nLockTime-using transactions that can be mined in the next
@@ -4143,8 +4143,7 @@ bool IsBlockMutated(const CBlock& block, bool check_witness_root)
         //
         // Note: This is not a consensus change as this only applies to blocks that
         // don't have a coinbase transaction and would therefore already be invalid.
-        return std::any_of(block.vtx.begin(), block.vtx.end(),
-                           [](auto& tx) { return GetSerializeSize(TX_NO_WITNESS(tx)) == 64; });
+        return std::ranges::any_of(block.vtx, [](auto& tx) { return tx->SizeNoWitness() == 64; });
     } else {
         // Theoretically it is still possible for a block with a 64 byte
         // coinbase transaction to be mutated but we neglect that possibility
1146ab21aca4d6b16cb43c53b4ff8d26f2222787
willcl-ark <will@256k1.dev>
2025-10-29 00:04:54 +0000

    guix: build static
    Essentially picked (into bench-ci/guix) from
    https://github.com/bitcoin/bitcoin/pull/25573
    
    Co-authored-by: fanquake <fanquake@gmail.com>

--- bench-ci/guix/manifest.scm
+++ bench-ci/guix/manifest.scm
@@ -438,6 +438,7 @@ inspecting signatures in Mach-O binaries.")
                   "--enable-standard-branch-protection=yes",
                   "--enable-cet=yes",
                   "--disable-gcov",
+                  "--disable-libsanitizer",
                   building-on)))
         ((#:phases phases)
           `(modify-phases ,phases
@@ -492,6 +493,37 @@ inspecting signatures in Mach-O binaries.")
                     (string-append out "/etc/rpc" suffix "\n"))
                    (("^install-others =.*$")
                     (string-append "install-others = " out "/etc/rpc\n")))))))))))))
+
+(define-public glibc-2.42
+  (let ((commit "71874f167aa5bb1538ff7e394beaacee28ebe65f"))
+  (package
+    (inherit glibc) ;; 2.39
+    (version "2.42")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://sourceware.org/git/glibc.git")
+                    (commit commit)))
+              (file-name (git-file-name "glibc" commit))
+              (sha256
+               (base32
+                "1pfbk907fkbavg7grbvb5zlhd3y47f8jj3d2v1s5w7xjnn0ypigq"))
+              (patches (search-our-patches "glibc-2.42-guix-prefix.patch"))))
+    (arguments
+      (substitute-keyword-arguments (package-arguments glibc)
+        ((#:configure-flags flags)
+          `(append ,flags
+            ;; https://www.gnu.org/software/libc/manual/html_node/Configuring-and-compiling.html
+            (list "--enable-stack-protector=all",
+                  "--enable-bind-now",
+                  "--enable-fortify-source",
+                  "--enable-cet=yes",
+                  "--enable-nscd=no",
+                  "--enable-static-nss=yes",
+                  "--disable-timezone-tools",
+                  "--disable-profile",
+                  "--disable-werror",
+                  building-on))))))))
 
 ;; The sponge tool from moreutils.
 (define-public sponge
@@ -560,6 +592,10 @@ inspecting signatures in Mach-O binaries.")
                  nsis-x86_64
                  nss-certs
                  osslsigncode))
+          ((string-contains target "x86_64-linux-")
+           (list (list gcc-toolchain-13 "static")
+                 (make-bitcoin-cross-toolchain target
+                                               #:base-libc glibc-2.42)))
           ((string-contains target "-linux-")
            (list bison
                  pkg-config
